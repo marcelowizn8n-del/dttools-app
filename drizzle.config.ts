a92b1b7a86fbe3bf358a98adeaaa29e214ecdb76
MFA <marcelowiz.n8n@gmail.com>
2025-10-05 22:56:57 -0300

    Add files via upload

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,8 +6,8 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
   out: "./migrations",
-  schema: "./shared/schema-sqlite.ts",
-  dialect: "sqlite",
+  schema: "./shared/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
